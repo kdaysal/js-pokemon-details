08b67d696176df3b5609b6d47fa4aed033fa3f6b
Kevin Daysal <kdaysal123@gmail.com>
2022-02-04 20:46:31 -0500

    Created pokemon list placeholder array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,39 @@
-alert('Hello world');
+let pokemonList = [{
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: [
+        'grass', 'poison'
+    ]
+},
+{
+    name: 'Sandslash',
+    height: 1,
+    types: ['ground']
+},
+{
+    name: 'Squirtle',
+    height: 0.5,
+    types: ['water']
+},
+{
+    name: 'Arbok',
+    height: 3.5,
+    types: ['poison']
+},
+{
+    name: 'Kakuna',
+    height: 0.6,
+    types: [
+        'bug', 'poison'
+    ]
+},
+{
+    name: 'Sharpedo',
+    height: 1.8,
+    types: [
+        'dark', 'water'
+    ]
+}
+];
 
-let favFood = 'BBQ Ribs';
-document.write(`My favorite food is: ${favFood}`);
+
