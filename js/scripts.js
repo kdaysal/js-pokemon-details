6b86edead429a700ddf301ac56a8025ee70fadfa
Kevin Daysal <kdaysal123@gmail.com>
2022-02-10 22:13:43 -0500

    Validated parameters in add() function - (bonus task)

--- js/scripts.js
+++ js/scripts.js
@@ -44,7 +44,13 @@ let pokemonRepository = (function () {
 
     //add new item to pokemonList
     function add(item) {
-        pokemonList.push(item);
+        if ((typeof (item) === 'object') && ((Object.keys(item)[0]) === 'name') && ((Object.keys(item)[1]) === 'height')) {
+            pokemonList.push(item);
+        }
+
+        else {
+            console.log('invalid data type');
+        }
     }
 
     return {
@@ -57,7 +63,16 @@ let wowMessage = "Wow, that's big!";
 
 //pokemonRepository.getAll() returns the full pokemonList array - then use forEach() to iterate through each pokemon object
 pokemonRepository.getAll().forEach((pokemon) => {
+
     //write pokemon heights and names to the document and add wowMessage for tallest pokemon
     return (pokemon.height > 3) ? document.write(`<p>${pokemon.name} (height: ${pokemon.height}m) - ${wowMessage}</p>`) : document.write(`<p>${pokemon.name} (height: ${pokemon.height}m)</p>`);
 });
 
+//code below this line is just for testing - DELETE LATER
+/*
+pokemonRepository.add({ name: 'Kevin', height: 1.4 });
+pokemonRepository.add({ name: 'Another Kevin', height: 7 });
+pokemonRepository.add({ namezzz: 'DONT ADD', height: 'DONT ADD' });
+pokemonRepository.add({ name: 'DONT ADD', heightzzz: 'DONT ADD' });
+console.log(pokemonRepository.getAll());
+*/
